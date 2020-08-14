573d6248f6565f27930585b5cad37072835e2074
AguPereyra <apereyra404@alumnos.iua.edu.ar>
2020-08-14 17:38:20 -0300

    Tostada agregada

--- helloworld_from0/app/src/main/java/org/gugu/example/helloworld_from0/MainActivity.kt
+++ helloworld_from0/app/src/main/java/org/gugu/example/helloworld_from0/MainActivity.kt
@@ -1,6 +1,28 @@
 package org.gugu.example.helloworld_from0
 
+import android.os.Bundle
+import android.view.Gravity
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Show toast on creation and pause
+        showLaTostada()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        // Show toast on creation and pause
+        showLaTostada()
+    }
+
+    private fun showLaTostada() {
+        val laTostada = Toast.makeText(baseContext, R.string.la_tostada, Toast.LENGTH_LONG)
+        laTostada.show()
+    }
 }
