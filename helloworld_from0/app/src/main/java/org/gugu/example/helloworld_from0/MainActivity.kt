ec6784164f423d9100dd44cd6f3666ad01bfbd4e
AguPereyra <apereyra404@alumnos.iua.edu.ar>
2020-08-27 08:56:31 -0300

    Snackbar

--- helloworld_from0/app/src/main/java/org/gugu/example/helloworld_from0/MainActivity.kt
+++ helloworld_from0/app/src/main/java/org/gugu/example/helloworld_from0/MainActivity.kt
@@ -2,8 +2,11 @@ package org.gugu.example.helloworld_from0
 
 import android.os.Bundle
 import android.view.Gravity
+import android.view.View
+import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,6 +18,15 @@ class MainActivity : AppCompatActivity() {
         showLaTostada()
         // Manually set title
         supportActionBar?.setTitle(R.string.app_title)
+        // Set snackbar to button click
+        val mainButton = findViewById<Button>(R.id.main_button)
+        mainButton.setOnClickListener {
+            val snack = Snackbar.make(it, "Behold the Snackbar!", Snackbar.LENGTH_LONG)
+            snack.setAction("Chau", View.OnClickListener {
+                System.out.println("Adios mundo curel...")
+            })
+            snack.show()
+        }
     }
 
     override fun onPause() {
@@ -24,7 +36,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun showLaTostada() {
-        val laTostada = Toast.makeText(applicationContext, R.string.la_tostada, Toast.LENGTH_LONG)
+        val laTostada = Toast.makeText(baseContext, R.string.la_tostada, Toast.LENGTH_LONG)
         laTostada.setGravity(Gravity.TOP ,400,0)
         laTostada.show()
 
