26d6ef7d0b7ef251a1f8a4eb68909f7cdcfec9dc
AguPereyra <apereyra404@alumnos.iua.edu.ar>
2020-08-29 16:52:57 -0300

    Navegacion main->second

--- Intents/app/src/main/java/org/gugu/example/intents/MainActivity.kt
+++ Intents/app/src/main/java/org/gugu/example/intents/MainActivity.kt
@@ -1,17 +1,30 @@
 package org.gugu.example.intents
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+
+    private val loginOnClickListener: View.OnClickListener = object : View.OnClickListener {
+        override fun onClick(v: View?) {
+            login()
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_main)
+        val btnSubmit = findViewById<Button>(R.id.submit_btn)
+        btnSubmit.setOnClickListener(loginOnClickListener)
     }
 
     fun login() {
-        //TODO: add functionality
+        val secondPageIntent = Intent(this, SecondActivity::class.java)
+        startActivity(secondPageIntent)
     }
 }
